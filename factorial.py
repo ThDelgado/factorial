c64f7d69023c7477b5d47871ae3b79ccf1fd26e2
ThDelgado <100494591+ThDelgado@users.noreply.github.com>
2024-08-15 09:45:40 -0400

    creacion de archivo

---
+++ factorial.py
@@ -0,0 +1,21 @@
+ 
+
+i = 0 
+
+num = 0 
+
+while i <= 4: 
+
+    print ("el numero es: " ,num) 
+
+    i += 1   
+
+    num = (num + i) * i 
+
+    if i == 4: 
+
+        break 
+
+    print (num) 
+
+         
